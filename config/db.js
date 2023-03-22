06ed9ef28b8926fbea0eab81f7cc16d799ec7ce3
Diki Gambol <westlakewetland@gmail.com>
2023-03-22 15:11:14 +0700

    updates

--- config/db.js
+++ config/db.js
@@ -1,20 +1,10 @@
-// import { Sequelize } from "sequelize";
-
-// const db = new Sequelize('sql12607679', 'sql12607679', 'K5ye74mY6f', {
-//     host: 'sql12.freesqldatabase.com',
-//     dialect: 'mysql',
-//     define: {
-//         timestamps: false
-//     },
-//     logging: console.log,
-// });
-
-// export default db;
-
 import { Sequelize } from "sequelize";
 
 const db = new Sequelize('db_tambang', 'root', '', {
     host: 'localhost',
+// jika mau pakai db online, tapi sering mati-mati xixixi
+// const db = new Sequelize('sql12607679', 'sql12607679', 'K5ye74mY6f', {
+//     host: 'sql12.freesqldatabase.com',
     dialect: 'mysql',
     define: {
         timestamps: false
